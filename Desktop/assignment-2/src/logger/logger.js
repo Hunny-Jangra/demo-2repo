49046d1ab266b40736060dc8d65f9e46db1cc77e
hunny <hunnympi@gmail.com>
2022-08-05 10:50:37 -0700

    Third commit

---
+++ Desktop/assignment-2/src/logger/logger.js
@@ -0,0 +1,8 @@
+const name = "Hunny"
+
+let Welcome = () => {
+    console.log('Welcome to my application. I am',name,'and a part of pluonium cohort');
+}
+
+module.exports.name = name
+module.exports.Welcome = Welcome
